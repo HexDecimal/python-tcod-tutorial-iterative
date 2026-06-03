842b6f6491b56354c2d380a7ed8be9af235772e7
Kyle Benesch <4b796c65+github@gmail.com>
2026-06-02 20:00:23 -0700

    Implement primitive player movement
    Simple as possible part 1 implementation for absolute beginners

--- main.py
+++ main.py
@@ -16,14 +16,26 @@ tileset = tcod.tileset.load_tilesheet("assets/terminal8x12_gs_ro.png", 16, 16, t
 def main() -> None:
     """Main entry point."""
     console = tcod.console.Console(90, 40)
-    console.print(x=0, y=0, text="Hello World")
+    player_x = console.width // 2
+    player_y = console.height // 2
 
     with tcod.context.new(console=console, tileset=tileset, title=TITLE) as context:
         while True:
+            console.clear()
+            console.print(x=player_x, y=player_y, text="@")
             context.present(console)
             for event in tcod.event.wait():
-                if isinstance(event, tcod.event.Quit):
-                    raise SystemExit
+                match event:
+                    case tcod.event.Quit():
+                        raise SystemExit
+                    case tcod.event.KeyDown(sym=tcod.event.KeySym.UP):
+                        player_y -= 1
+                    case tcod.event.KeyDown(sym=tcod.event.KeySym.DOWN):
+                        player_y += 1
+                    case tcod.event.KeyDown(sym=tcod.event.KeySym.LEFT):
+                        player_x -= 1
+                    case tcod.event.KeyDown(sym=tcod.event.KeySym.RIGHT):
+                        player_x += 1
 
 
 if __name__ == "__main__":
